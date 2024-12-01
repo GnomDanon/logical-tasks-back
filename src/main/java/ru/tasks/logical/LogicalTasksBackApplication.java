e3342f47934f95996a7d3010757a5594083d1ba6
Daniil <89221449094dg@gmail.com>
2024-12-01 13:47:47 +0500

    Инициализация проекта

---
+++ src/main/java/ru/tasks/logical/LogicalTasksBackApplication.java
@@ -0,0 +1,13 @@
+package ru.tasks.logical;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogicalTasksBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LogicalTasksBackApplication.class, args);
+    }
+
+}
