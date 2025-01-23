49fdbaaac4fad0cdacad5ce054256e24f9312e85
Daniil <89221449094dg@gmail.com>
2025-01-24 03:42:35 +0500

    FIX Получение информации о задаче

--- src/main/java/ru/tasks/logical/task/dto/Task.java
+++ src/main/java/ru/tasks/logical/task/dto/Task.java
@@ -3,11 +3,13 @@ package ru.tasks.logical.task.dto;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 import lombok.experimental.Accessors;
+import ru.tasks.logical.task.generator.dto.questions.Question;
 
 @Data
 @Accessors(chain = true)
 @Schema(description = "Задача")
 public class Task {
 	TaskInfo taskInfo;
+	Question[] question;
 	String content;
 }
--- src/main/java/ru/tasks/logical/task/service/TaskService.java
+++ src/main/java/ru/tasks/logical/task/service/TaskService.java
@@ -2,14 +2,21 @@ package ru.tasks.logical.task.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import ru.tasks.logical.common.dto.CrosswordQuestionItem;
+import ru.tasks.logical.common.dto.TestItem;
 import ru.tasks.logical.task.converter.CrosswordAttributeConverter;
 import ru.tasks.logical.task.converter.CrosswordConverter;
+import ru.tasks.logical.task.converter.CrosswordQuestionsAttributeConverter;
+import ru.tasks.logical.task.converter.TestQuestionsAttributeConverter;
 import ru.tasks.logical.task.dto.Task;
 import ru.tasks.logical.task.dto.TaskInfo;
 import ru.tasks.logical.task.dto.TaskTypeInfo;
 import ru.tasks.logical.task.entity.Crossword;
+import ru.tasks.logical.task.entity.CrosswordQuestions;
 import ru.tasks.logical.task.entity.TaskType;
+import ru.tasks.logical.task.entity.TestQuestions;
 import ru.tasks.logical.task.exception.TaskNotExistsException;
+import ru.tasks.logical.task.generator.dto.questions.Question;
 import ru.tasks.logical.task.mapper.TaskMapper;
 import ru.tasks.logical.task.mapper.TaskTypeMapper;
 import ru.tasks.logical.task.repository.TaskRepository;
@@ -26,6 +33,8 @@ public class TaskService {
     private final TaskRepository taskRepository;
     private final CrosswordConverter crosswordConverter = new CrosswordConverter();
     private final CrosswordAttributeConverter crosswordAttributeConverter = new CrosswordAttributeConverter();
+    private final TestQuestionsAttributeConverter testQuestionsConverter = new TestQuestionsAttributeConverter();
+    private final CrosswordQuestionsAttributeConverter crosswordQuestionsConverter = new CrosswordQuestionsAttributeConverter();
 
     public List<TaskInfo> getTasksByAuthorId(UUID authorId) {
         return taskRepository.findAllByAuthor_Id(authorId)
@@ -44,18 +53,41 @@ public class TaskService {
         ru.tasks.logical.task.entity.Task task = taskRepository.findById(taskId).orElseThrow(TaskNotExistsException::new);
 
         TaskInfo taskInfo = TaskMapper.mapTaskToTaskInfo(task);
-        String content = "";
+        String content = task.getContent();
+        String questions = task.getQuestions();
         TaskType taskType = task.getTaskType();
+        Question[] taskQuestions = new Question[task.getMaxScore()];
 
-        switch (taskType) {
-            case TEST -> content = task.getContent();
-            case CROSSWORD -> {
-                String taskContent = task.getContent();
-                Crossword crossword = crosswordAttributeConverter.convertToEntityAttribute(taskContent);
-                content = crosswordConverter.convertToString(crossword);
+        if (content != null) {
+            switch (taskType) {
+                case TEST -> content = task.getContent();
+                case CROSSWORD -> {
+                    String taskContent = task.getContent();
+                    Crossword crossword = crosswordAttributeConverter.convertToEntityAttribute(taskContent);
+                    content = crosswordConverter.convertToString(crossword);
+                }
             }
         }
 
-        return new Task().setTaskInfo(taskInfo).setContent(content);
+        if (questions != null) {
+            switch (taskType) {
+                case TEST -> {
+                    TestQuestions testQuestions = testQuestionsConverter.convertToEntityAttribute(questions);
+                    for (int i = 0; i < testQuestions.getQuestions().length; i++) {
+                        TestItem testItem = testQuestions.getQuestions()[i];
+                        taskQuestions[i] = Question.test(testItem.getQuestion(), testItem.getAnswers(), testItem.getCorrectAnswer());
+                    }
+                }
+                case CROSSWORD -> {
+                    CrosswordQuestions crosswordQuestions = crosswordQuestionsConverter.convertToEntityAttribute(questions);
+                    for (int i = 0; i < crosswordQuestions.getQuestions().length; i++) {
+                        CrosswordQuestionItem crosswordItem = crosswordQuestions.getQuestions()[i];
+                        taskQuestions[i] = Question.crossword(crosswordItem.getQuestion(), crosswordItem.getAnswer());
+                    }
+                }
+            }
+        }
+
+        return new Task().setTaskInfo(taskInfo).setContent(content).setQuestion(taskQuestions);
     }
 }
